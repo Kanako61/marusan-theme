81606d3d2fd16c09c4d0d33601cf1a0315d29cfb
Kanako61 <kana.v3v.moco@gmail.com>
2024-05-15 21:34:03 +0900

    一旦保存

--- contact.php
+++ contact.php
@@ -1,16 +1,16 @@
-<section class="contact">
-  <div class="contact_inner">
+<section class="contact-box">
+  <div class="contact-box_inner">
   <h2 class="top_h2">お問い合わせ</h2>
 
-  <p class="contact_text">プレカット加工依頼や、お見積もりご希望の方<br class="pc_br">工場見学を希望する方は、お電話か下記メールフォームよりご連絡下さい</p>
+  <p class="contact-box_text">プレカット加工依頼や、お見積もりご希望の方<br class="pc_br">工場見学を希望する方は、お電話か下記メールフォームよりご連絡下さい</p>
 
-  <div class="contact_content">
-    <div class="contact_tel">
+  <div class="contact-box_content">
+    <div class="contact-box_tel">
       <p>平日 8:00〜17:00<br>(祝日・年末年始を除く)</p>
       <p class="tel_num">0243-55-2626</p>
     </div>
 
-    <div class="contact_mail">
+    <div class="contact-box_mail">
       <a href="<?php echo home_url('/'); ?>contact">メールでのお問い合わせ</a>
     </div>
   </div>
